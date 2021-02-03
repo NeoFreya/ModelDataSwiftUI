d3625dd10b8e48b566f91ebf83e244a70edb7e5e
NeoFreya <neonblackcat19@gmail.com>
2021-02-03 10:50:33 +0800

    Initial Commit

---
+++ ModelDataSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModelDataSwiftUI
+//
+//  Created by Abdul Halim on 03/02/21.
+//  Copyright © 2021 NEONFACT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
